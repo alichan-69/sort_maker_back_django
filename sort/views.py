aec1e88b0ecb8806919b6e86ff9e089ca13205e2
alichan-69 <alichan.work@gmail.com>
2022-04-08 23:27:48 +0900

    userのapi作成

--- sort/views.py
+++ sort/views.py
@@ -1,12 +1,23 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, permissions
 
 from .models import Sort
 from .serializer import SortSerializer
 
 
+class UserPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method == 'DELETE':
+            body = request.data
+            b_user_id = body['user_id']
+            return False
+        return True
+
+
 class SortViewSet(viewsets.ModelViewSet, mixins.DestroyModelMixin):
     queryset = Sort.objects.all()
     serializer_class = SortSerializer
+    permission_classes = (UserPermission, )
 
     def perform_destroy(self, instance):
         instance.delete_flg = True
