cded94beb0f6db1852d5b558abc0b04ec8c16a9f
alichan-69 <alice.hitugi@gmail.com>
2022-04-11 23:20:25 +0900

    destroyを途中まで作成

--- sort/views.py
+++ sort/views.py
@@ -1,24 +1,33 @@
-from rest_framework import viewsets, mixins, permissions
-
-from .models import Sort
 from .serializer import SortSerializer
+from .models import Sort
+from rest_framework import viewsets, mixins
 
+from common.response import create_api_response
+from common.validation import check_exist_key, check_str_out_of_range
+from common.format import date_format
 
-class UserPermission(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        if request.method == 'DELETE':
-            body = request.data
-            b_user_id = body['user_id']
-            return False
-        return True
+import datetime
 
 
 class SortViewSet(viewsets.ModelViewSet, mixins.DestroyModelMixin):
     queryset = Sort.objects.all()
     serializer_class = SortSerializer
-    permission_classes = (UserPermission, )
 
-    def perform_destroy(self, instance):
-        instance.delete_flg = True
-        instance.save
+    def destroy(self, request, pk=None):
+        body = request.data
+
+        # ポストされたデータの必須キーの存在チェック
+        required_keys = ["user_id"]
+        check_exist_key(required_keys, body)
+
+        # ポストされたデータを変数に格納
+        b_user_id = body["user_id"]
+
+        # その他データベースに登録する値を変数に格納
+        delete_flg = True
+        update_date = date_format(datetime.datetime.now())
+
+        # バリデーション
+        check_str_out_of_range(b_user_id, 1, 255)
+
+        return create_api_response(200, "OK")
