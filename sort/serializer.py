9202c121389edb03f3c0bc0f0bbcf894069ac432
alichan-69 <alichan.work@gmail.com>
2022-05-12 03:38:28 +0900

    sortItemのモデル登録、API作成

--- sort/serializer.py
+++ sort/serializer.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from .models import Sort
+from .models import Sort, SortItem
 
 
 class SortSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sort
         fields = ('id', 'name', 'description', 'image', 'user_id',
+                  'delete_flg', 'create_date', 'update_date')
+
+
+class SortItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SortItem
+        fields = ('id', 'name', 'image', 'sort',
                   'delete_flg', 'create_date', 'update_date')
